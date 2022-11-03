aa93503650d201dc3414349482f95fd8e50188fd
franciscopulidocal <franciscopulidocal@gmail.com>
2022-11-03 17:46:38 -0300

    endpoint to get all transactions added

--- src/main/java/com/alkemy/wallet/service/ITransactionService.java
+++ src/main/java/com/alkemy/wallet/service/ITransactionService.java
@@ -1,7 +1,11 @@
 package com.alkemy.wallet.service;
 
+import java.util.List;
+
 import com.alkemy.wallet.model.entity.Transaction;
 
 public interface ITransactionService {
     public void saveTransaction(Transaction transaction);
+
+    public List<Transaction> findAllByAccountId(Long accountId);
 }
--- src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/alkemy/wallet/service/impl/TransactionServiceImpl.java
@@ -3,6 +3,10 @@ package com.alkemy.wallet.service.impl;
 import com.alkemy.wallet.model.entity.Transaction;
 import com.alkemy.wallet.repository.TransactionRepository;
 import com.alkemy.wallet.service.ITransactionService;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +16,21 @@ public class TransactionServiceImpl implements ITransactionService {
     private TransactionRepository transactionRepository;
 
     @Override
-    public void saveTransaction(Transaction transaction){
+    public void saveTransaction(Transaction transaction) {
         transactionRepository.save(transaction);
+    }
+
+    @Override
+    public List<Transaction> findAllByAccountId(Long accountId) {
+        List<Transaction> allTransactions = transactionRepository.findAll();
+        List<Transaction> accountTransactions = new ArrayList<>();
+
+        for (Transaction transaction : allTransactions) {
+            if (transaction.getAccount().getAccountId() == accountId) {
+                accountTransactions.add(transaction);
+            }
+        }
+
+        return accountTransactions;
     }
 }
