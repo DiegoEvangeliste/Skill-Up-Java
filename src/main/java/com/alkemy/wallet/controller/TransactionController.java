d8c867fe72a57901860c9d703205620dc65c12b0
franciscopulidocal <franciscopulidocal@gmail.com>
2022-11-04 13:19:44 -0300

    adding transaction list endpoint

--- src/main/java/com/alkemy/wallet/controller/TransactionController.java
+++ src/main/java/com/alkemy/wallet/controller/TransactionController.java
@@ -4,15 +4,15 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.alkemy.wallet.model.entity.Transaction;
 import com.alkemy.wallet.service.ITransactionService;
 
-@Controller
+@RestController
 @RequestMapping("/transactions")
 public class TransactionController {
     @Autowired
